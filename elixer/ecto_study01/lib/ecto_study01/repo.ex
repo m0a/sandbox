db7a6646c2fe0b30dc8abdc5a32dda83cd35c976
makoto abe <abe00makoto@gmail.com>
2016-07-16 20:34:55 +0900

    ぬるい勉強メモを削除

--- elixer/ecto_study01/lib/ecto_study01/repo.ex
+++
@@ -1,3 +0,0 @@
-defmodule EctoStudy01.Repo do
-  use Ecto.Repo, otp_app: :ecto_study01
-end
--- elixer/ecto_study01/test/ecto_study01_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EctoStudy01Test do
-  use ExUnit.Case
-  doctest EctoStudy01.App
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
